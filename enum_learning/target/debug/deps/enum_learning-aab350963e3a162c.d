97156d91f8086b18056df69c475d6bc26c7bea97
João Mantovani <joaomantovani12345@gmail.com>
2024-10-21 19:51:28 -0300

    Learning how to use Enum and extract the information from it

---
+++ enum_learning/target/debug/deps/enum_learning-aab350963e3a162c.d
@@ -0,0 +1,5 @@
+C:\Users\joaom\Desktop\Learning_Rust\enum_learning\target\debug\deps\enum_learning-aab350963e3a162c.exe: src/main.rs
+
+C:\Users\joaom\Desktop\Learning_Rust\enum_learning\target\debug\deps\enum_learning-aab350963e3a162c.d: src/main.rs
+
+src/main.rs:
---
+++ enum_learning/target/debug/enum_learning.d
@@ -0,0 +1,1 @@
+C:\Users\joaom\Desktop\Learning_Rust\enum_learning\target\debug\enum_learning.exe: C:\Users\joaom\Desktop\Learning_Rust\enum_learning\src\main.rs
