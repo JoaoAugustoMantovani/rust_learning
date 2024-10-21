934cef1d8fd5b74ef941c26e1fba39994d85baa3
João Mantovani <joaomantovani12345@gmail.com>
2024-10-21 19:58:41 -0300

    Exploring Vectors and their functions

---
+++ vector/target/debug/deps/vector-f1e39698b036bef1.d
@@ -0,0 +1,5 @@
+C:\Users\joaom\Desktop\Learning_Rust\vector\target\debug\deps\vector-f1e39698b036bef1.exe: src/main.rs
+
+C:\Users\joaom\Desktop\Learning_Rust\vector\target\debug\deps\vector-f1e39698b036bef1.d: src/main.rs
+
+src/main.rs:
---
+++ vector/target/debug/vector.d
@@ -0,0 +1,1 @@
+C:\Users\joaom\Desktop\Learning_Rust\vector\target\debug\vector.exe: C:\Users\joaom\Desktop\Learning_Rust\vector\src\main.rs
